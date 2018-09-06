1c8a45ec85d52bc24b22aef424b647aa431c9137
DrDeathBubbles <aaron.meagher@gmail.com>
2018-09-06 11:26:47 +0100

    Adding CreateClient to the categories list

--- video/categories.py
+++ video/categories.py
@@ -8,6 +8,23 @@ import gdata.docs.data
 import gdata.sample_util
 
 
+def CreateClient():
+  """Create a Documents List Client."""
+  client = gdata.docs.client.DocsClient(source=SampleConfig.APP_NAME)
+  client.http_client.debug = SampleConfig.DEBUG
+  # Authenticate the user with CLientLogin, OAuth, or AuthSub.
+  try:
+    gdata.sample_util.authorize_client(
+        client,
+        service=client.auth_service,
+        source=client.source,
+        scopes=client.auth_scopes
+    )
+  except gdata.client.BadAuthentication:
+    exit('Invalid user credentials given.')
+  except gdata.client.Error:
+    exit('Login Error')
+  return client
 
 
 
