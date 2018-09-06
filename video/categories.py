d94ed293b28455c65b135a5220a3884e6f8a0e2f
DrDeathBubbles <aaron.meagher@gmail.com>
2018-09-06 11:26:21 +0100

    Adding in imports

--- video/categories.py
+++ video/categories.py
@@ -1,5 +1,16 @@
 # Sample python code for videoCategories.list
 
+import os.path
+import gdata.data
+import gdata.acl.data
+import gdata.docs.client
+import gdata.docs.data
+import gdata.sample_util
+
+
+
+
+
 def video_categories_list_for_region(client, **kwargs):
   # See full sample for function
   kwargs = remove_empty_kwargs(**kwargs)
@@ -14,3 +25,7 @@ video_categories_list_for_region(client,
     part='snippet',
     hl='es',
     regionCode='ES')
+
+
+if __name__ == '__main__':
+    client = 
